a01dc6adecb38f052eb0feae71c6a5163ba373b6
joon <joon.durandt@gmail.com>
2022-12-04 12:19:33 +0000

    Simplified code for Day 4

--- day4.py
+++ day4.py
@@ -1,18 +1,18 @@
 file = open('day4_input.txt', 'r')
 lines = [s.strip() for s in file.readlines()]
 
-def expand_assignment(assignment):
+def parse_assignment(assignment):
     parts = assignment.split('-')
-    return list(range(int(parts[0]), int(parts[1]) + 1))
+    return [int(parts[0]), int(parts[1])]
 
 def parse_assignments(assignments):
     parts = assignments.split(',')
-    return (expand_assignment(parts[0]), expand_assignment(parts[1]))
+    return (parse_assignment(parts[0]), parse_assignment(parts[1]))
 
 def overlaps(left, right):
-    if left[0] >= right[0] and left[0] <= right[-1] and left[-1] >= right[0] and left[-1] <= right[-1]:
+    if left[0] >= right[0] and left[-1] <= right[-1]:
         return True
-    if right[0] >= left[0] and right[0] <= left[-1] and right[-1] >= left[0] and right[-1] <= left[-1]:
+    if right[0] >= left[0] and right[-1] <= left[-1]:
         return True
     return False
 
