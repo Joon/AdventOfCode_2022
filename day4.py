c1ef0855f908badc1cf2ed9746721b6a4ee75862
joon <joon.durandt@gmail.com>
2022-12-04 12:21:06 +0000

    Cleanup

--- day4.py
+++ day4.py
@@ -10,20 +10,20 @@ def parse_assignments(assignments):
     return (parse_assignment(parts[0]), parse_assignment(parts[1]))
 
 def overlaps(left, right):
-    if left[0] >= right[0] and left[-1] <= right[-1]:
+    if left[0] >= right[0] and left[1] <= right[1]:
         return True
-    if right[0] >= left[0] and right[-1] <= left[-1]:
+    if right[0] >= left[0] and right[1] <= left[1]:
         return True
     return False
 
 def partial_overlap(left, right):
-    if (left[0] >= right[0] and left[0] <= right[-1]) or (right[0] >= left[0] and right[0] <= left[-1]):
+    if (left[0] >= right[0] and left[0] <= right[1]) or (right[0] >= left[0] and right[0] <= left[1]):
         return True
     return False
 
 assignments = [parse_assignments(l) for l in lines]
 overlaps = [a for a in assignments if overlaps(a[0], a[1])]
-print(len(overlaps))
+print("Part 1: {}".format(len(overlaps)))
 
 partial_overlaps = [a for a in assignments if partial_overlap(a[0], a[1])]
-print(len(partial_overlaps))
+print("Part 2: {}".format(len(partial_overlaps)))
